f988517a0e3163ce8cce815fc0778de068a19dfd
aileenunez <88999968+aileenunez@users.noreply.github.com>
2021-11-26 18:49:19 -0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect} from 'react'
+
+//Make two API calls  
+//Make HTML foundation 
+//Make Search Bar Component 
+//Make a Footer Component 
+//Make a Bottom Nav 
+//Router/Routes/Route 
+//set up Use State, Use Effect 
+//Incorporate Firebase 
 
 function App() {
+    useEffect(() => {
+      const apiKeyMov = `786c1383f2a24f7ee0f7ae525d2a9af4`
+      axios({
+        url: "https://api.themoviedb.org/3/search/movie",
+        params: {
+          api_key: apiKeyMov, 
+          query: `Toy Story 2`, 
+        }
+      }).then((response) => {
+        console.log(response.data.results)
+      })
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
